98f4a5daddbfc31f1cb6c1144ee53d086fdee483
sjk <649937897@qq.com>
2022-09-23 17:12:37 +0800

    feat:项目初始化

--- src/icons/index.js
+++ src/icons/index.js
@@ -5,5 +5,11 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 Vue.component('svg-icon', SvgIcon)
 
 const req = require.context('./svg', false, /\.svg$/)
+// console.log(req.keys())
+// console.log(req('./dashboard.svg'))
+// console.log(require.context('./svg', false, /\.svg$/)('./dashboard.svg'))
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
+// console.log(requireAll(req))
+// console.log(req => req.keys().map(req))
+// console.log(require.context('./svg', false, /\.svg$/).keys().map(require.context('./svg', false, /\.svg$/)))
