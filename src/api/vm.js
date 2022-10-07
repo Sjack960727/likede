e37b6a167a82bd56f6f2ef2f499e758047ac8355
Antidote666 <www.25868@qq.com>
2022-10-07 17:48:35 +0800

    debug

--- src/api/vm.js
+++ src/api/vm.js
@@ -227,3 +227,47 @@ export const getVendingListOperators = (innerCode) => {
     url: `/user-service/user/operatorList/` + innerCode
   })
 }
+
+export const getSkuTop = (topValue, start, end) => {
+  return request({
+    url: `/order-service/report/skuTop/${topValue}/${start}/${end}`
+  })
+}
+
+export const getSales = (start, end, innerCode) => {
+  return request({
+    url: `/order-service/report/orderCount`,
+    params: {
+      start, end,
+      innerCode
+    }
+  })
+}
+
+export const getIncome = (start, end, innerCode) => {
+  return request({
+    url: `/order-service/report/orderAmount`,
+    params: {
+      start, end,
+      innerCode
+    }
+  })
+}
+
+export const amountCollect = (collectType, start, end) => {
+  return request({
+    url: `/order-service/report/amountCollect/${collectType}/${start}/${end}`
+  })
+}
+
+export const regionCollect = (start, end) => {
+  return request({
+    url: `/order-service/report/regionCollect/${start}/${end}`
+  })
+}
+
+export const obtainTheSalesVolume = (innerCode, start, end) => {
+  return request({
+    url: `/order-service/report/skuCollect/${innerCode}/${start}/${end}`
+  })
+}
