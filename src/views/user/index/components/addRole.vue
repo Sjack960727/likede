cd1f522915e14a340bce04a8c2bb3ec1cf5e378d
sjk <649937897@qq.com>
2022-10-07 19:01:48 +0800

    1901

--- src/views/user/index/components/addRole.vue
+++ src/views/user/index/components/addRole.vue
@@ -136,13 +136,13 @@ export default {
     beforeAvatarUpload(file) {
       const isJPG = file.type === 'image/jpeg'
       const isLt2M = file.size / 1024 < 100
-
       if (!isJPG) {
         this.$message.error('上传头像图片只能是 JPG 格式!')
       }
       if (!isLt2M) {
         this.$message.error('上传头像图片大小不能超过 100kb!')
       }
+      console.log(123)
       return isJPG && isLt2M
     },
     async getRegion() {
