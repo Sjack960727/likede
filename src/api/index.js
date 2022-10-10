e20be7752db60b574fd2d48de125d81e9ae621d5
sjk <649937897@qq.com>
2022-10-10 08:30:47 +0800

    1010

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,5 @@ export * from './login'
 export * from './user'
 export * from './dashboard'
 export * from './vm'
-<<<<<<< HEAD
 export * from './policy'
-=======
 export * from './report'
->>>>>>> feature/report
